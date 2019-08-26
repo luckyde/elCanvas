1f0c71814810b09553f484a44811d249da64da18
Lucky <ldee@zynga.com>
2019-08-26 15:34:29 -0400

    work in progress build of new image loading methods
    using el.importImages to import images as well as url parameters

--- _tests/_word_seeker_logo/js/el_animation.js
+++ _tests/_word_seeker_logo/js/el_animation.js
@@ -1,8 +1,23 @@
 function el_animation(el, params){
 
+  //input
+  var imageURLs=[
+    {id:'h2',url:'images/heading_2.png'}  ,
+    {id:'h3',url:'images/heading_3.png'}  ,
+    {id:'h4',url:'images/heading_4.png'}  ,
+    {id:'s1',url:'images/subheading_1.png'}  ,
+    {id:'s2',url:'images/subheading_2.png'}  ,
+    {id:'s3',url:'images/subheading_3.png'}  ,
+    {id:'s4',url:'images/subheading_4.png'}  ,
+    {id:'s5',url:'images/subheading_5.png'}  ,
+    {id:'s6',url:'images/subheading_6.png'}
+  ]
+  //import
+  el.importImages(imageURLs);
+  //draw
 // gradients
 var center={x:el.width/2,y:el.height/2}
-
+// console.log(imageURLs);
 var strokeGrd = el.ctx.createLinearGradient(0,200,100,300);
     strokeGrd.addColorStop(0, '#1A0A7E');
     strokeGrd.addColorStop(1, '#A737A1');
@@ -16,70 +31,55 @@ var strokeGrd = el.ctx.createLinearGradient(0,200,100,300);
         radGrd.addColorStop(1, 'rgba(0,3,187,0.75)');
 
   // CREATION
-  var outer_blue_big= new el.rect({width:411,height:411,x:45,y:45,radius:75,opacity:0.75,style:{fillStyle:"#93E2F9"}})
-  var outer_blue_medium= new el.rect({width:380,height:380,x:60,y:60,radius:75,opacity:0.7,style:{fillStyle:"#2DB3E8"}})
-  var outer_blue_small= new el.rect({width:335,height:335,x:82.5,y:82.5,radius:50, style:{fillStyle:"#018FD9"}})
+  var outer_blue_big= el.rect({width:411,height:411,x:45,y:45,radius:75,opacity:0.75,style:{fillStyle:"#93E2F9"}})
+  var outer_blue_medium= el.rect({width:380,height:380,x:60,y:60,radius:75,opacity:0.7,style:{fillStyle:"#2DB3E8"}})
+  var outer_blue_small= el.rect({width:335,height:335,x:82.5,y:82.5,radius:50, style:{fillStyle:"#018FD9"}})
 
-  var tiled_sh_big= new el.rect({width:345,height:345,x:77,y:77,rotation:45,radius:50,opacity:0.5,style:{fillStyle:"#3741B7"}})
-  var tiled_sh_big_stroke= new el.rect({x:75,y:75,width:350,height:350,radius:50,rotation:45,style:{lineWidth:3.5,strokeStyle:strokeGrd,fillStyle:'rgba(0,0,0,0)'}})
+  var tiled_sh_big= el.rect({width:345,height:345,x:77,y:77,rotation:45,radius:50,opacity:0.5,style:{fillStyle:"#3741B7"}})
+  var tiled_sh_big_stroke= el.rect({x:75,y:75,width:350,height:350,radius:50,rotation:45,style:{lineWidth:3.5,strokeStyle:strokeGrd,fillStyle:'rgba(0,0,0,0)'}})
 
-  var tiled_sh_medium= new el.rect({width:291,height:291,x:104.5,y:104.5,radius:50,rotation:45,style:{fillStyle:radGrd}})
-  var tiled_sh_small= new el.rect({width:199,height:199,x:150,y:150,radius:40,rotation:45,style:{fillStyle:'#032A8C'}})
-
-
-  var outer= [outer_blue_big,outer_blue_medium,outer_blue_small]
-
-  var inner= [tiled_sh_big,tiled_sh_big_stroke,tiled_sh_medium,tiled_sh_small]
-
-  // var text = new el.text({y:100,x:200,zIndex:4,text:'sdfkfsd',style:{ fillStyle:"#ff0000",font:"63px museo500"}})
-
+  var tiled_sh_medium= el.rect({width:291,height:291,x:104.5,y:104.5,radius:50,rotation:45,style:{fillStyle:radGrd}})
+  var tiled_sh_small= el.rect({width:199,height:199,x:150,y:150,radius:40,rotation:45,style:{fillStyle:'#032A8C'}})
+  var outer= [outer_blue_big,outer_blue_medium,outer_blue_small];
+  var inner= [tiled_sh_big,tiled_sh_big_stroke,tiled_sh_medium,tiled_sh_small];
   var heading = [
-     new el.image({id:'h1',x:16,y:149,zIndex:25,transformOrigin:"50% 50%"}),
-    new el.image({id:'h2',x:175,y:157,zIndex:24,transformOrigin:"45% 40%"}),
-   new el.image({id:'h3',x:278,y:152 ,zIndex:23,transformOrigin:"50% 50%"}),
-    new el.image({id:'h4',x:374,y:161,zIndex:22,transformOrigin:"30% 50%"}),
+    el.image({url:'images/heading_1.png',x:16,y:149,zIndex:25,transformOrigin:"50% 50%"}),
+    el.image({id:'h2',x:175,y:157,zIndex:24,transformOrigin:"45% 40%"}),
+    el.image({id:'h3',x:278,y:152 ,zIndex:23,transformOrigin:"50% 50%"}),
+    el.image({id:'h4',x:374,y:161,zIndex:22,transformOrigin:"30% 50%"}),
   ]
 
   var subheading = [
-    new el.image({id:'s1',x:93,y:297,transformOrigin:"50% 98%"}),
-    new el.image({id:'s2',x:147,y:298,transformOrigin:"50% 98%"}),
-    new el.image({id:'s3',x:196,y:298,transformOrigin:"50% 98%"}),
-    new el.image({id:'s4',x:246,y:298,transformOrigin:"50% 98%"}),
-    new el.image({id:'s5',x:306,y:298,transformOrigin:"50% 98%"}),
-    new el.image({id:'s6',x:356,y:298,transformOrigin:"50% 98%"}),
+    el.image({id:'s1',x:93,y:297,transformOrigin:"50% 98%"}),
+    el.image({id:'s2',x:147,y:298,transformOrigin:"50% 98%"}),
+    el.image({id:'s3',x:196,y:298,transformOrigin:"50% 98%"}),
+    el.image({id:'s4',x:246,y:298,transformOrigin:"50% 98%"}),
+    el.image({id:'s5',x:306,y:298,transformOrigin:"50% 98%"}),
+    el.image({id:'s6',x:356,y:298,transformOrigin:"50% 98%"}),
   ]
 
-  // var nose = new el.image({id:'nose',x:150,y:200,scale:1,parent:head,transformOrigin:"50% 50%"})
+  // function animate(){
+    var heading_tl =  new TimelineMax()
+      .set(subheading,{scaleX:1,scaleY:1,opacity:1},0)
+      .staggerFrom(heading,1,{scaleX:4,scaleY:0.3,ease:Elastic.easeOut.config(0.6,0.2)},0.1,0)
+      .staggerFrom(heading,0.9,{y:600,ease:Elastic.easeOut.config(0.5,0.3)},0.1,0.1)
+      .staggerFrom(heading,0.02,{opacity:0,ease:Power3.reaseOut},0.1,0.2)
 
-  //
-  // var circ = new el.circle()
-  // var rect = new el.rect({id:'rect',parent:circ})
-  // var rect2 = new el.rect({id:'rect2',parent:circ})
-// nose.rotation=90;
-var heading_tl =  new TimelineMax()
-.set(subheading,{scaleX:1,scaleY:1,opacity:1},0)
-.staggerFrom(heading,1,{scaleX:4,scaleY:0.3,ease:Elastic.easeOut.config(0.6,0.2)},0.1,0)
-.staggerFrom(heading,0.9,{y:600,ease:Elastic.easeOut.config(0.5,0.3)},0.1,0.1)
-.staggerFrom(heading,0.02,{opacity:0,ease:Power3.reaseOut},0.1,0.2)
-var easeOut = new TimelineMax()
+    var easeOut = new TimelineMax()
+      .staggerTo([outer,inner],0.8,{scale:0.001,rotation:45,ease:Elastic.easeIn.config(0.4,0.3)},0.1,0)
+      .staggerTo([heading,subheading],0.58,{scaleX:0,scaleY:0,y:200,x:250,opacity:0,ease:Elastic.easeIn.config(0.4,0.3)},0.02,0.2)
 
-.staggerTo([outer,inner],0.8,{scale:0.001,rotation:45,ease:Elastic.easeIn.config(0.4,0.3)},0.1,0)
-.staggerTo([heading,subheading],0.58,{scaleX:0,scaleY:0,y:200,x:250,opacity:0,ease:Elastic.easeIn.config(0.4,0.3)},0.02,0.2)
-  var bot_message_tl= new TimelineMax({onUpdate:el.update})
-  .staggerFrom(outer,1,{rotation:40,ease:Elastic.easeOut.config(0.5,0.3),scale:0.6,opacity:0},0.1,0)
-  .staggerFrom(inner,1,{rotation:90,ease:Elastic.easeOut.config(0.5,0.5),opacity:0},0.1,0.1)
-.add(heading_tl,0.1)
-.staggerFrom(subheading,1,{y:"-=150",scaleX:0,ease:Elastic.easeOut.config(0.5,0.2)},0.05,0.6)
-.add(easeOut,3)
-  // .to(subheading[1],1,{repeat:-1,scaleY:0,ease: Linear.easeNone})
-  // .to(heading[3],1,{rotation:360,repeat:-1,ease: Linear.easeNone})
-  // .to(rect3,10,{x:200}, 0)
-  // .to(deleteme,10,{y:200},0)
-  // .add(function(){el.remove(deleteme)},1)
-  // .to(rect,1,{radius:20,ease:Linear.easeNone},0)
-    // .to(nose,0.3,{y:40,ease:Back.easeOut,repeat:-1,yoyo:true},0)
+      var bot_message_tl= new TimelineMax({onUpdate:el.update})
+        .staggerFrom(outer,1,{rotation:40,ease:Elastic.easeOut.config(0.5,0.3),scale:0.6,opacity:0},0.1,0)
+        .staggerFrom(inner,1,{rotation:90,ease:Elastic.easeOut.config(0.5,0.5),opacity:0},0.1,0.1)
+        .add(heading_tl,0.1)
+        .staggerFrom(subheading,1,{y:"-=150",scaleX:0,ease:Elastic.easeOut.config(0.5,0.2)},0.05,0.6)
+        .add(easeOut,3)
 
+        return bot_message_tl;
+  // }
+  // el.update()
 
+    // el.onImagesReady(animate);
 
-  return bot_message_tl;
 }
--- _tests/_word_seeker_logo/js/setup.js
+++ _tests/_word_seeker_logo/js/setup.js
@@ -3,49 +3,19 @@ function gE(e){ return document.getElementById(e)};
 var canvas_container;
 
 function setup(){
-  // resize canvas to parent width and height, work around so the canvas size doesnt need to be hard typed in html
   canvas_container = document.getElementById('elCanvas');
-   var imageURLs=[
-     {id:'h1',url:'images/heading_1.png'}  ,
-     {id:'h2',url:'images/heading_2.png'}  ,
-     {id:'h3',url:'images/heading_3.png'}  ,
-     {id:'h4',url:'images/heading_4.png'}  ,
-     {id:'s1',url:'images/subheading_1.png'}  ,
-     {id:'s2',url:'images/subheading_2.png'}  ,
-     {id:'s3',url:'images/subheading_3.png'}  ,
-     {id:'s4',url:'images/subheading_4.png'}  ,
-     {id:'s5',url:'images/subheading_5.png'}  ,
-     {id:'s6',url:'images/subheading_6.png'}  
-  ]
-  var images = el_imageLoad(imageURLs,animate);
+  this.el = new elCanvas(canvas_container, {AlignToCenter:false});
+  var test_tl = new el_animation(this.el);
 
-};
-function animate(props){
+  // test_tl.eventCallback("onUpdate", drawFrame);
+  // function drawFrame(event) {
+  //
+  //   // this is required in order to update the canvas
+  //   el.update();
+  //
+  //
+  // }
 
-  // edit this to edit the content on the page
-  var dynamicInfo = {
-    test_value:"Test123",
-  }
-  // setup canvas information
-  /* create a el_canvas reference, which has the same properties as a canvas
-  but allows chaining and has extra properties to help speed up drawing */
-
-  this.el = new elCanvas(canvas_container, {images:props,AlignToCenter:false});
-
-  // create custom shapes for future use
-  this.customShapes = new el_util_custom_shapes(this.el);
-
-  var test_tl = new el_animation(this.el,dynamicInfo);
-
-  test_tl.eventCallback("onUpdate", drawFrame);
-  function drawFrame(event) {
-
-    // this is required in order to update the canvas
-    el.update();
-
-
-  }
-
-  canvas_container.addEventListener('click',function(){ test_tl.play(0);});
+  // canvas_container.addEventListener('click',function(){ test_tl.play(0);});
 
 }
