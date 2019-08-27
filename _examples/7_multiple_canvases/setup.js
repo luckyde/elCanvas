8c9dd392222bf8f309a5ea063de42c12121bb405
Lucky <ldee@zynga.com>
2019-08-27 13:10:33 -0400

    added calback just in case situation covers

--- _examples/7_multiple_canvases/setup.js
+++ _examples/7_multiple_canvases/setup.js
@@ -10,12 +10,12 @@ function animate(props){
   //Works
   var chosenCanvas = el_b;
   //Doesn't work
-  // var chosenCanvas = el_a;
+  var chosenCanvas = el_a;
   // create custom tile
   var makeTile = function(params,canvas) {
     var shell=canvas.rect({
     transformOrigin:"-00% 20%",zIndex:2,id:params.letter,numb:params.value,width:90,height:90,style:{fillStyle:"rgba(0,0,0,0)"}});
-    var bg_storke=canvas.rect({scale:0,radius:23,width:90,height:90,x:-90/2,y:-90/2,parent:shell,id:'tile',style:{fillStyle:'blue'}});
+    var bg_storke=  canvas.rect({scale:0,radius:23,width:90,height:90,x:-90/2,y:-90/2,parent:shell,id:'tile',style:{fillStyle:'blue'}});
     var bgStyle =     {fillStyle:'red',strokeStyle:'blue',lineWidth:6}
     var bg=canvas.rect({width:90,height:90,x:-90/2,y:-90/2,parent:shell,radius:22,id:'tile',name:params.letter,style:bgStyle});
     var letter=canvas.text({transformOrigin:"-50% 0%",parent:bg,x:43,y:73,text:params.letter, style:{font:"65px museo700",  fillStyle:"#4C1900"}})
