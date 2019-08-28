8fcd2497e96b66dbc062ec9b01d2465aa6044fa0
Lucky <ldee@zynga.com>
2019-08-28 11:42:34 -0400

    added ability to import via image object ,same system as before

--- _examples/3_images/setup.js
+++ _examples/3_images/setup.js
@@ -1,10 +1,8 @@
 // the images need to be loaded first as base64 before anything else happens
-var imageURLs=[
-   {id:'nose',url:'nose.png'}
-]
-var images = el_imageLoad(imageURLs,animate);
-function animate(props){
-  console.log(props);
+// var imageURLs=[
+//    {id:'nose',url:'nose.png'}
+// ]
+function setup(){
   var starShape = function(){
         el.ctx.beginPath()
         .moveTo(0,17).lineTo(17,15).lineTo(24,0)
@@ -17,21 +15,26 @@ function animate(props){
   //declare  where the canvas goes
   var canvas_container = document.getElementById('elCanvas');
   // create an instance
-  this.el = new elCanvas(canvas_container,{images:props,AlignToCenter:true});
-  // create an object
-  var rectangle = new el.rect({y:-300,width:300,x:-200,height:200})
-  var circle = new el.circle({x:-90})
-  var circle2 = new el.circle({x:90})
+  this.el = new elCanvas(canvas_container,{AlignToCenter:true});
 
-  // images need to be preloaded into the images list as base64 for them to work
-  var image = new el.image({id:'nose'})
-  console.log(this.el);
-  var shape = new el.shape({x:0,y:120,customShape:starShape,scale:2})
-  // a custom shape must be specified otherwise you will get errors 
-  // create a timeline, have the canvas update on update
-  var tl = new TimelineMax({onUpdate: el.update})
+  
+  // importing via image object
+  el.importImages({head:'head.png'})
+  var imageViaID = el.image({id:'head'})
+  //instancing test via ID
+  var imageViaID_instance = el.image({x:-400,id:'head'})
 
+  //importing via url
+  var imageViaURL = el.image({x:170,y:240,id:'nose',url:'nose.png'})
+  //instancing test via URL
+  var imageViaURL_instance = el.image({x:170-400,y:240,id:'nose'})
 
+  function animate(){
+      var tl = new TimelineMax({onUpdate: el.update})
+  }
+
+  //when using images, you should wait for the images to load in order to start animating
+  el.ready().then(animate);
 
   // link for interactive animation
   canvas_container.addEventListener('click',function(){ test_tl.play(0);});
